f402a5e99db1e281f4b6d39c10edb275563c2543
carollcotriim <ana.cotrim@iesb.edu.br>
2024-08-27 21:48:49 -0300

    AULAAAAAAAA

---
+++ 03-react-fundamentos/src/app/fundamentos/NumeroMaior.js
@@ -0,0 +1,12 @@
+
+
+export default function NumeroMaior() {
+
+    return (
+        <>
+        <h2>Componente Número maior</h2>
+        <p>O número maior é:</p>
+        <p>Propriedades Recebidas: {propriedades.teste}</p>
+        </>
+    )
+}
---
+++ 03-react-fundamentos/src/app/fundamentos/page.js
@@ -0,0 +1,21 @@
+import MeuComponente from "./MeuComponente"
+import NumeroMaior from "./NumeroMaior"
+
+
+
+
+export default function Fundamentos() {
+    return (
+
+        <>
+        {/*Comentário no JSX*/}
+            <h1>Página Fundamentos</h1>
+            
+            <MeuComponente/>
+            <hr/>
+
+              
+            
+        </>
+    )
+} 
